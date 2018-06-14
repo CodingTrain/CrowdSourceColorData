685d32bd1935b0fa953767bdb837f86aa7a33372
Eric Paulson <Paulson.Eric@Gmail.com>
2018-06-14 01:17:28 -0700

    Layout and color selection changes.
    Don't display the RGB values to the user to discourage "cheating".
    
    The color is presented to the user twice side by side, once on a black
    background and once on a white background. The background of the page
    is a gray color midway between black and white. This seems to improve
    color distinction.
    
    The buttons have been reordered to more closely align with the color
    spectrum.
    
    Choosing from random RGB values biases the colors towards green-ish.
    Instead, we want to uniformly sample from all hues.
    We bias towards full saturation or else too many colors look either
    gray,  brown, or pink.
    We bias away from full black and full white primarily because they
    aren't  options presented to the user.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CrowdSourceColorData
-For crowd sourcing color data for a machine learning classification example
+For crowd sourcing color data for a machine learning classification example.  You can contribute your choices [here](https://codingtrain.github.io/CrowdSourceColorData/).
 
 Loading icon created at [https://loading.io/](https://loading.io/).
--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
 let r, g, b;
 let database;
-let rgbDiv;
 
 function pickColor() {
-  r = floor(random(256));
-  g = floor(random(256));
-  b = floor(random(256));
-  background(r, g, b);
-  rgbDiv.html(`R:${r} G:${g} B:${b}`);
+  /*
+    Choosing from random RGB values biases the colors towards green-ish.
+    Instead, we want to uniformly sample from all hues.
+    We bias towards full saturation or else too many colors look either gray,
+    brown, or pink.
+    We bias away from full black and full white primarily because they aren't
+    options presented to the user.
+   */
+
+  colorMode(HSL);
+  let h = random(360);
+  let s = randomGaussian(100, 35);
+  let l = randomGaussian(50, 15);
+  while (s < 0 || s > 100)
+    s = randomGaussian(100, 35);
+  while (l < 0 || l > 100)
+    l = randomGaussian(50, 15);
+
+  let c = color(h, s, l);
+
+  colorMode(RGB);
+  r = round(red(c));
+  g = round(green(c));
+  b = round(blue(c));
+
+  // Create background.
+  rectMode(CORNER);
+  fill(0);
+  rect(0, 0, width / 2, height);
+  fill(255);
+  rect(width / 2, 0, width / 2, height);
+
+  // Lay out swatches side by side.
+  rectMode(CENTER);
+  fill(r, g, b);
+  rect(width * 0.25, height / 2, width / 2 * 0.7, height * 0.7);
+  rect(width * 0.75, height / 2, width / 2 * 0.7, height * 0.7);
   hideLoading();
 }
 
@@ -24,20 +55,18 @@ function setup() {
   firebase.initializeApp(config);
   database = firebase.database();
 
-
-  createCanvas(100, 100).parent('#root');
-  rgbDiv = createDiv().parent('#root');
-  
+  createCanvas(480, 240).parent('#root');
+  noStroke();
   pickColor();
 
   let buttons = [];
   buttons.push(createButton('red-ish').parent('#root').class('red-ish'));
-  buttons.push(createButton('green-ish').parent('#root').class('green-ish'));
-  buttons.push(createButton('blue-ish').parent('#root').class('blue-ish'));
   buttons.push(createButton('orange-ish').parent('#root').class('orange-ish'));
   buttons.push(createButton('yellow-ish').parent('#root').class('yellow-ish'));
-  buttons.push(createButton('pink-ish').parent('#root').class('pink-ish'));
+  buttons.push(createButton('green-ish').parent('#root').class('green-ish'));
+  buttons.push(createButton('blue-ish').parent('#root').class('blue-ish'));
   buttons.push(createButton('purple-ish').parent('#root').class('purple-ish'));
+  buttons.push(createButton('pink-ish').parent('#root').class('pink-ish'));
   buttons.push(createButton('brown-ish').parent('#root').class('brown-ish'));
   buttons.push(createButton('grey-ish').parent('#root').class('grey-ish'));
 
@@ -54,9 +83,9 @@ function sendData() {
 
   // Make an object with data in it
   var data = {
-    r: r,
-    g: g,
-    b: b,
+    r,
+    g,
+    b,
     label: this.html()
   }
   console.log('saving data');
