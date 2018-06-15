3daafe89a7856f5cc083ece9ac4a12c6df6fbd76
Max <maximpely@gmail.com>
2018-06-15 13:18:51 +0100

    removed data cleaning js from main page

--- submit.js
+++ submit.js
@@ -73,18 +73,6 @@ function setup() {
       length_div.html(`${length} data entries so far`);
       length_div_made = true
   });
-
-  // Commenting out the loading of data for the webpage running
-  // console.log("Retreiving data... (this can take a minute or two)");
-  // loadData().then(data => {
-  //   dataSave = data;
-  //   console.log("Recieved data. To analyze", data.length, "entries, run: ");
-  //   console.log("showSample(dataSave, 'red-ish')");
-  //   console.log("or analyzeData(dataSave, ['red-ish', 'blue-ish'])");
-  //   console.log("To clean the data by label and hue use: ");
-  //   console.log("let green_data = cleanData(dataSave, 'green-ish', 60, 180)");
-  //   console.log("For any help, please see the documentation above each function in the code!");
-  // });
 }
 
 async function sendData() {
@@ -126,99 +114,6 @@ async function sendData() {
       setTimeout(hideLoading, 600);
     }
   }
-}
-
-
-/** Produce a filtered version of the input data.
- *   First, all data whose label does not match 'name' is discarded.
- *   Then, all data must encode a RGB color which has a hue
- *   value greater than minHue and less than maxHue.
- *   Special case!
- *   If minHue > maxHue, the range wraps around the 360->0 hue gap.
- * @function cleanData
- * @param {Array} data - returned by loadData(), saved in dataSave
- * @param {string} name - the label to produce clean data for
- * @param {number} minHue - 0 <= minHue <= 360. Lower limit of hue range
- * @param {number} maxHue - 0 <= maxHue <= 360. Upper limit of hue range
- * @return {Array} Your squeeky clean data!
- * @example let green_data = cleanData(dataSave, 'green-ish', 60, 180)
- * @example let red_data = cleanData(dataSave, 'red-ish', 300, 60)
- */
-function cleanData(data, name, minHue, maxHue) {
-  const entries = filterData(data, name);
-  console.log("Cleaning", entries.length, "entries for", name);
-  let result = [];
-  for (let entry of entries) {
-    let { r, g, b } = entry;
-    let h = hue(color(r, g, b));
-    if (minHue < h && h < maxHue) {
-      result.push(entry);
-    } else if (minHue > maxHue && (minHue < h || h < maxHue)) {
-      result.push(entry);
-    }
-  }
-  console.log("Result contains", result.length, "entries.");
-  return result;
-}
-
-/** Actually draw on the canvas as many colors from that
- *   label as possible, with one pixel for each color.
- * @function showSample
- * @param {Array} data - returned by loadData(), saved in dataSave
- * @param {Array} name - name of the label to draw, ex. "blue-ish"
- * @return {undefined}
- * @example showSample(dataSave, 'green-ish')
- */
-function showSample(data, name) {
-  const entries = filterData(data, name);
-  console.log("Found", entries.length, "entries for", name);
-
-  let img = createImage(width, height);
-  let d = pixelDensity();
-  img.loadPixels();
-  for (let i = 0; i < width * height * d && i < entries.length; i++) {
-    let { r, g, b } = entries[i];
-    img.set(i % width, floor(i / height), color(r, g, b));
-  }
-  img.updatePixels();
-
-  background(255);
-  image(img, 0, 0);
-}
-
-/** Show hue metrics for colors of the data.
- * @async
- * @function analyzeData
- * @param {Array} data - returned by loadData(), saved in dataSave
- * @param {Array} colors - color labels to analyze
- * @return {undefined}
- * @example analyzeData(data, buttons.map(e=>e.html()))
- */
-function analyzeData(data, colors) {
-  for (name of colors) {
-    const entries = filterData(data, name);
-    console.log("Found", entries.length, "entries for", name);
-    let avgHue = 0;
-    let validCount = 0;
-    for (let { r, g, b } of entries) {
-      let h = hue(color(r, g, b));
-      avgHue += h;
-      validCount++;
-    }
-    avgHue /= validCount;
-    console.log("Average", name, "hue: ", avgHue);
-  }
-}
-
-function filterData(data, name) {
-  return data.filter(({ label, r, g, b }) => label === name && Number.isInteger(r) && Number.isInteger(g) && Number.isInteger(b));
-}
-
-function loadData() {
-  return database
-    .ref("/colors/")
-    .once("value")
-    .then(snapshot => Object.values(snapshot.val()));
 }
 
 function showLoading() {
